10636617a2e687b8563fcbac07d8d9fabf79bac3
nevillejodie <jlneville92@gmail.com>
2020-07-18 19:57:43 +0100

    accordion added

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactPlayer from 'react-player/youtube'
 import css from './home.module.css'
+import Example from '../Projects'
 
 function Home() {
-    return (<div className={css.wrapper}><h2>Jodie Neville, Software Developer</h2>;
-    <ReactPlayer  url='https://youtu.be/GRnnmIiAJ0U' /></div>)
+    return (<div className={css.wrapper}><h2>Jodie Neville, Software Developer</h2>
+    <p>Watch my introductory video for FemTechConf to learn a bit more about me:</p>;
+    <ReactPlayer  url='https://youtu.be/GRnnmIiAJ0U' />
+    <h2>Projects</h2>
+    <Example/>
+   </div>)
   }
 
   export default Home
---
+++ src/Components/Projects/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+ 
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+} from 'react-accessible-accordion';
+ 
+// Demo styles, see 'Styles' section below for some notes on use.
+import 'react-accessible-accordion/dist/fancy-example.css';
+ 
+export default function Example() {
+    return (
+        <Accordion allowZeroExpanded="true">
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        Code First: Girls Website
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>
+                        Exercitation in fugiat est ut ad ea cupidatat ut in
+                        cupidatat occaecat ut occaecat consequat est minim minim
+                        esse tempor laborum consequat esse adipisicing eu
+                        reprehenderit enim.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        WMCA Contract Management App
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>
+                        As part of the School of Code Bootcamp we were tasked by the West Midlands Combined Authority
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+            <AccordionItem>
+                <AccordionItemHeading>
+                    <AccordionItemButton>
+                        Bootcamper Network App
+                    </AccordionItemButton>
+                </AccordionItemHeading>
+                <AccordionItemPanel>
+                    <p>
+                        In ad velit in ex nostrud dolore cupidatat consectetur
+                        ea in ut nostrud velit in irure cillum tempor laboris
+                        sed adipisicing eu esse duis nulla non.
+                    </p>
+                </AccordionItemPanel>
+            </AccordionItem>
+        </Accordion>
+    );
+}

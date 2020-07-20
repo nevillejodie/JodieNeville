77b49023f7ca1c4227874b752e1a9c8fe756ba7b
nevillejodie <jlneville92@gmail.com>
2020-07-20 15:11:54 +0100

    added into about me

--- src/Components/AboutMe/index.js
+++ src/Components/AboutMe/index.js
@@ -6,7 +6,10 @@ function About() {
     return ( <div className={css.wrapper}><h2>Jodie Neville, Software Developer</h2>
     <div className={css.flier}><img alt="avatar made for School of Code" src={Avatar}/></div>
     
-      <p className={css.p}>I am a Birmingham-based developer trained through the 4-month, full-time intensive School of Code bootcamp. My switch to coding is thanks to the Code First: Girls Intro to Web Development course. Having previously managed student experience projects and initiatives at the University of Birmingham, I am interested in using code to creatively solve problems and build positive user and developer experiences. I regularly attend Code First: Girls, Codebar and Brum JS meetups. I really enjoy being a part of the technology community in Birmingham. My long-term mission is to utilise technology for good and give back to the communities that have supported my own journey into tech. </p>
+      <p className={css.p}>I am a Birmingham-based developer trained through the 4-month, full-time intensive School of Code bootcamp. My switch to coding is thanks to the Code First: Girls Intro to Web Development course. Having previously managed student experience projects and initiatives at the University of Birmingham, I am interested in using code to creatively solve problems and build positive user and developer experiences. </p>
+      <p>I have now come full circle and am volunteering as a remote instructor on the Code First: Girls Introduction to Web Development course. It is so fulfilling to be able to give others the same opportunity that I had. On this course I teach HTML, CSS, JavaScript, project design and management, GitHub and version control. I have received come really complimentary feedback on my teaching and I enjoy it thoroughly!</p>
+      <p>I had the opportunity to present our final bootcamp project to React London: Bring Your Own Project meetup. I really enjoyed speaking at this event and look forward to doing it more in the future. </p>
+      <p>I regularly attend Code First: Girls, Codebar and Brum JS meetups. I really enjoy being a part of the technology community in Birmingham. My long-term mission is to utilise technology for good and give back to the communities that have supported my own journey into tech. </p>
       </div>)
   }
 
--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -4,9 +4,15 @@ import css from './contact.module.css'
 function Contact() {
     return (<div className={css.wrapper}><h2>Get in touch</h2>
       <p>I would be particularly receptive to hearing about job, speaking, or teaching opportunities in Tech. Get in touch on the following channels: </p>
+      <a href="https://twitter.com/JodieNeville" rel="noopener noreferrer" target="_blank">
       <button className={css.btn}><img height="25px" alt="twitter icon" src="https://i.ibb.co/cJw1VZt/transp-twitter.png"/> @JodieNeville</button>
+      </a>
+      <a href="https://github.com/nevillejodie" rel="noopener noreferrer" target="_blank">
       <button className={css.btn}><img height="25px" alt="github icon" src="https://i.ibb.co/hgW1VBh/transp-github-better.png"/> @nevillejodie</button>
+      </a>
+      <a href="https://www.linkedin.com/in/jlneville/" rel="noopener noreferrer" target="_blank">
       <button className={css.btn}><img height="25px" alt="linkedin icon" src="https://i.ibb.co/MMxKZ0T/transp-linked-in.png"/> @jlneville</button>
+      </a>
       </div>)
   }
 

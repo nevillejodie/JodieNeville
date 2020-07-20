7b37d180eb472a469fcaeff26864282601cbdf7e
nevillejodie <jlneville92@gmail.com>
2020-07-20 14:24:14 +0100

    project info and screenshots

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
- 
+import bhub from '../../Images/bhub.png'
+import bootcamp from '../../Images/bootcamper menu.png'
+import bootcamp2 from '../../Images/bootcamper network.png'
+import bootcamp3 from '../../Images/bootcamper profile.png'
 import {
     Accordion,
     AccordionItem,
@@ -22,11 +25,19 @@ export default function Example() {
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        Exercitation in fugiat est ut ad ea cupidatat ut in
-                        cupidatat occaecat ut occaecat consequat est minim minim
-                        esse tempor laborum consequat esse adipisicing eu
-                        reprehenderit enim.
+                    This course combined education with a network of motivated individuals. It provided an opportunity to put the skills learnt into practice by working with a group to create a website. The skills included:
+<ul><li>Use of HTML, CSS, and Javascript.</li>
+<li>Git, Github and Version Control.</li> 
+<li>Bootstrap and JQuery.</li>
+<li>Consideration for User Experience (UX).</li></ul>
+
+Our group won the competition for the website we created. The website can be viewed <a href="https://nevillejodie.github.io/group_project/home.html">here</a>.   
+
+The feedback for our website was as follows: <br></br>
+"This website is gorgeous, I absolutely love the design and it was great that you backed up your design decisions in your presentation by referencing your target audience. I was impressed by how you've gone the extra mile to source fonts and think through your UX.
+The interactive elements of the website were very impressive. The hover and drop-down features meant it already felt dynamic and engaging even in your limited time. It was clear in your presentation that you'd worked well together as a team and made collaborative decisions. Well done!!!!"
                     </p>
+                    <img height="250px" alt="screenshot of bhub website" src={bhub}></img>
                 </AccordionItemPanel>
             </AccordionItem>
             <AccordionItem>
@@ -37,7 +48,7 @@ export default function Example() {
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        As part of the School of Code Bootcamp we were tasked by the West Midlands Combined Authority
+                    Our team of three had 1 week to build and present solutions to a problem posed to us by the West Midlands Combined Authority on managing their contract process more efficiently using technology. We built a full-stack solution, represented on the front-end in a React App with form and automated email functionality. We completed this project using the agile methodology, having regular stand-ups and communicating effectively as a team. I worked on the front-end using state to manage the logic for our React components and ensuring fetches from and posts to our database were effective. You can view the repos on my <a href="https://github.com/nevillejodie">GitHub</a>. 
                     </p>
                 </AccordionItemPanel>
             </AccordionItem>
@@ -49,10 +60,11 @@ export default function Example() {
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                        In ad velit in ex nostrud dolore cupidatat consectetur
-                        ea in ut nostrud velit in irure cillum tempor laboris
-                        sed adipisicing eu esse duis nulla non.
+                    For our four-week final project our team built a ‘one stop shop’ for information and communications relating to the School of Code network of bootcampers, mentors, and employers. We used React, made it a PWA, interfaced with the Meetup API using the OAuth strategy, used web sockets for our chat feature, authenticated logins using firebase, hosted our PostgreSQL database using AWS RDS and AWS Elastic Beanstalk. We used technologies we were familiar with but also pushed ourselves to learn new ones. We worked in an agile way, breaking the build down into distinct phases and iterating through each of them. See the finished result <a href="https://bootcamper-network.netlify.app/">here</a>.   
                     </p>
+                    <img height="250px" alt="screenshot of bootcamp app" src={bootcamp3}></img>
+                    <img height="250px" alt="screenshot of bootcamp app" src={bootcamp}></img>
+                    <img height="250px" alt="screenshot of bootcamp app" src={bootcamp2}></img>
                 </AccordionItemPanel>
             </AccordionItem>
         </Accordion>

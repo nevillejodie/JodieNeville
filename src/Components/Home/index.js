2665b6dd13180526f7b419e596a07395e51452e7
nevillejodie <jlneville92@gmail.com>
2020-07-21 11:35:08 +0100

    got rid of the word website-unnecessary

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,7 @@ import css from './home.module.css'
 import Example from '../Projects'
 
 function Home() {
-    return (<div className={css.wrapper}><h2>Welcome to my portfolio website</h2>
+    return (<div className={css.wrapper}><h2>Welcome to my portfolio</h2>
     <p>Watch my introductory video for FemTechConf to learn a bit more about me:</p>;
     <ReactPlayer className={css.video} url='https://youtu.be/GRnnmIiAJ0U' />
     <h2>Projects</h2>

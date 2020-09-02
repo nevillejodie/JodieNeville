c825a2a8ad451518631fbd00c641f9cd6e6b2c22
nevillejodie <jlneville92@gmail.com>
2020-09-02 22:06:41 +0100

    edited form

--- src/Components/Mail/index.js
+++ src/Components/Mail/index.js
@@ -4,14 +4,17 @@ import Axios from 'axios'
 function Mail() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [text, setText] = useState('')
 
   const handleClick = (e) => {
     e.preventDefault();
 
     if(e.target.id === "name") {
       setName(e.target.value)
-    } else {
+    } else if (e.target.id === "email") {
       setEmail(e.target.value)
+    } else {
+      setText(e.target.value)
     }
   }
 
@@ -20,7 +23,8 @@ function Mail() {
 
     const dataToSubmit = {
       name,
-      email
+      email,
+    /*   text */
     }
   
 
@@ -28,12 +32,15 @@ Axios.post("http://localhost:5000/api/sendMail", dataToSubmit)
 
 setName('')
 setEmail('')
+/* setText('') */
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input id="name" placeholder="name" value={name} onChange={handleClick}/>
-        <input id="email" placeholder="email" value={email} onChange={handleClick}/>
+        <input id="name" placeholder="Include your name and a way to contact you" value={name} onChange={handleClick} style={{width: "350px"}}/>
+       {/*  <input id="email" placeholder="email" value={email} onChange={handleClick}/> */}
+        {/* <input id="text" placeholder="text" value={text} onChange={handleClick}/> */}
+        <br/>
         <button onClick={handleSubmit}>Send</button>
       </form>
     </div>

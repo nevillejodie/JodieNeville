e6a1a9866b5bbd942ecf1bf8352a7338a0cb5d80
nevillejodie <jlneville92@gmail.com>
2020-09-02 22:45:36 +0100

    deleted test for netlify

--- src/Components/Mail/index.js
+++ src/Components/Mail/index.js
@@ -4,27 +4,24 @@ import Axios from 'axios'
 function Mail() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
-  const [text, setText] = useState('')
+
 
   const handleClick = (e) => {
     e.preventDefault();
 
     if(e.target.id === "name") {
       setName(e.target.value)
-    } else if (e.target.id === "email") {
-      setEmail(e.target.value)
     } else {
-      setText(e.target.value)
-    }
+      setEmail(e.target.value)
+    } 
   }
-
   const handleSubmit = (e) => {
     e.preventDefault();
 
     const dataToSubmit = {
       name,
       email,
-    /*   text */
+
     }
   
 
@@ -32,14 +29,13 @@ Axios.post("http://localhost:5000/api/sendMail", dataToSubmit)
 
 setName('')
 setEmail('')
-/* setText('') */
+
   }
   return (
     <div>
       <form onSubmit={handleSubmit}>
         <input id="name" placeholder="Include your name and a way to contact you" value={name} onChange={handleClick} style={{width: "350px"}}/>
-       {/*  <input id="email" placeholder="email" value={email} onChange={handleClick}/> */}
-        {/* <input id="text" placeholder="text" value={text} onChange={handleClick}/> */}
+  
         <br/>
         <button onClick={handleSubmit}>Send</button>
       </form>

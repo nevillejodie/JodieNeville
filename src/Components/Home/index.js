d89dd85a5e76ebff8f08b2d4cdd1731c41743b73
nevillejodie <jlneville92@gmail.com>
2020-10-11 10:49:12 +0100

    deleted video

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,8 +6,7 @@ import Example from '../Projects'
 
 function Home() {
     return (<div className={css.wrapper}><h2>Welcome to my portfolio</h2>
-    <p>Watch my introductory video for FemTechConf to learn a bit more about me:</p>;
-    <ReactPlayer className={css.video} url='https://youtu.be/GRnnmIiAJ0U' />
+    
     <h2>Projects</h2>
     <Example/>
    </div>)

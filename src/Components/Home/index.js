ed400b70a2dd38920ce3c51a50968b15643735ae
nevillejodie <jlneville92@gmail.com>
2020-10-25 17:25:05 +0000

    added picture

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,7 @@ import Projects from '../Projects'
 
 
 function Home() {
-    return (<div className={css.wrapper}><h2 className={css.intro}>Hi, I'm Jodie.</h2>
+    return (<div className={css.wrapper}><h2 className={css.intro}>Hi, I'm Jodie.</h2><img className={css.prof} height="150px" alt="pic of me" src="https://pbs.twimg.com/profile_images/1315278321056448513/7eFQLBiZ_400x400.jpg"/>
     <p>
     I'm a Birmingham based woman in Tech. Click below to see some of the websites and applications I've built:</p>
     <h2>Projects</h2>

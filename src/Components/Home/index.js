e8e161cf41fec5547a5f67306856cdda8cd6cb12
nevillejodie <jlneville92@gmail.com>
2020-10-25 19:03:48 +0000

    changed the wording a little

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -6,7 +6,7 @@ import Projects from '../Projects'
 function Home() {
     return (<div className={css.wrapper}><h2 className={css.intro}>Hi, I'm Jodie.</h2><img className={css.prof} height="150px" alt="pic of me" src="https://pbs.twimg.com/profile_images/1315278321056448513/7eFQLBiZ_400x400.jpg"/>
     <p>
-    I'm a Birmingham based woman in Tech. Click below to see some of the websites and applications I've built:</p>
+     Learn more about me and explore some of the websites and applications I've built below.</p>
     <h2>Projects</h2>
     <Projects/>
    </div>)

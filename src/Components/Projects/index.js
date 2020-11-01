55421f7016ea4b34d68ff935ca83ebd09df0486c
nevillejodie <jlneville92@gmail.com>
2020-11-01 19:21:57 +0000

    sorted list problems in accordion section

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -32,7 +32,7 @@ export default function Projects() {
                 </AccordionItemHeading>
                 <AccordionItemPanel>
                     <p>
-                    For our four-week final project on the School of Code bootcamp our team built a ‘one stop shop’ for information and communications relating to the School of Code network. Here's some of the tech we used: <ul><li>React</li><li>Made it a Progressive Web App</li><li>Interfaced with the Meetup API using the OAuth strategy</li><li>Used web sockets for our chat feature</li><li>Authenticated logins using Firebase</li><li>Hosted our PostgreSQL database using AWS RDS and AWS Elastic Beanstalk</li></ul> We used technologies we were familiar with but also pushed ourselves to learn new ones. We worked in an agile way, breaking the build down into distinct phases and iterating through each of them. See the finished result <a className={css.link} href="https://bootcamper-network.netlify.app/">here</a>.   
+                    For our four-week final project on the School of Code bootcamp our team built a ‘one stop shop’ for information and communications relating to the School of Code network. Here's some of the tech we used: <ul><li>React</li><li>Made it a Progressive Web App</li><li>Interfaced with the Meetup API using the OAuth strategy</li><li>Used web sockets for our chat feature</li><li>Authenticated logins using Firebase</li><li>Hosted our PostgreSQL database using AWS RDS and AWS Elastic Beanstalk</li></ul> <p>We used technologies we were familiar with but also pushed ourselves to learn new ones. We worked in an agile way, breaking the build down into distinct phases and iterating through each of them. See the finished result <a className={css.link} href="https://bootcamper-network.netlify.app/">here</a>.</p>   
                     </p>
                     <figure><img className={css.img} height="250px" alt="screenshot of bootcamp app" src={bootcamp3}></img>
                     <figcaption>Creating a profile on the Bootcamper Network App.</figcaption></figure>
@@ -71,21 +71,18 @@ export default function Projects() {
                     </AccordionItemButton>
                 </AccordionItemHeading>
                 <AccordionItemPanel>
-                    <p>
-                    This course provided an opportunity to put the skills learnt into practice by working in a group to create a website. The skills included:
+                    <p>This course provided an opportunity to put the skills learnt into practice by working in a group to create a website. The skills included: </p>
 <ul><li>Use of HTML, CSS, and Javascript.</li>
 <li>Git, Github and Version Control.</li> 
 <li>Bootstrap and JQuery.</li>
 <li>Consideration for User Experience (UX).</li></ul>
 
-Our group won the competition for the website we created. The website can be viewed <a className={css.link} href="https://nevillejodie.github.io/group_project/home.html">here</a>.   
+<p>Our group won the competition for the website we created. The website can be viewed <a className={css.link} href="https://nevillejodie.github.io/group_project/home.html">here</a>.   </p>
 
-The feedback we received is as follows: 
+<p>The feedback we received is as follows:</p> 
 <p className={css.quote}>"This website is gorgeous, I absolutely love the design and it was great that you backed up your design decisions in your presentation by referencing your target audience. I was impressed by how you've gone the extra mile to source fonts and think through your UX.
 The interactive elements of the website were very impressive. The hover and drop-down features meant it already felt dynamic and engaging even in your limited time. It was clear in your presentation that you'd worked well together as a team and made collaborative decisions. Well done!!!!"</p>
-
-                    </p>
-                    <figure><img className={css.img} height="250px" alt="screenshot of bhub website" src={bhub}></img></figure>
+<figure><img className={css.img} height="250px" alt="screenshot of bhub website" src={bhub}></img></figure>
                     
                 </AccordionItemPanel>
             </AccordionItem>

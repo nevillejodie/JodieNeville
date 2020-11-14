ae50008c87e9bd168b615e1144cf98cda71a2f07
nevillejodie <jlneville92@gmail.com>
2020-11-14 18:28:38 +0000

    final amends for today

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -5,9 +5,9 @@ import Projects from '../Projects'
 
 function Home() {
     return (<div className={css.wrapper}><h2 className={css.intro}>Hi, I'm Jodie.</h2><img className={css.prof} height="150px" alt="pic of me" src="https://pbs.twimg.com/profile_images/1315278321056448513/7eFQLBiZ_400x400.jpg"/>
+    <div className={css.experiment}>
     <p className={css.p}>I am a Birmingham-based developer trained through the <a className={css.link} href="https://www.schoolofcode.co.uk/">School of Code</a> bootcamp. My switch to coding is thanks to the <a className={css.link} href="https://codefirstgirls.org.uk/">Code First: Girls</a>  Intro to Web Development course. Having previously managed student experience projects and initiatives at the University of Birmingham, I am interested in using code to creatively solve problems and build positive user and developer experiences. </p>
-    <p className={css.p}>
-     Learn more about me and explore some of the websites and applications I've built below.</p>
+    </div>
     <h2>Projects</h2>
     <Projects/>
    </div>)

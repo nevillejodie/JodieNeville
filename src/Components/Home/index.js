45285e54b3104af373eca06abe0c9228ec1242e4
nevillejodie <jlneville92@gmail.com>
2021-04-08 11:36:19 +0100

    rm bad pic

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -4,7 +4,7 @@ import Projects from '../Projects'
 
 
 function Home() {
-    return (<div className={css.wrapper}><h1 className={css.intro}>Hi, I'm Jodie.</h1><img className={css.prof} height="150px" alt="pic of me" src="https://pbs.twimg.com/profile_images/1315278321056448513/7eFQLBiZ_400x400.jpg"/>
+    return (<div className={css.wrapper}><h1 className={css.intro}>Hi, I'm Jodie.</h1>
     <div className={css.experiment}>
     <p className={css.p}>I am a Birmingham-based developer trained through the <a className={css.link} href="https://www.schoolofcode.co.uk/">School of Code</a> bootcamp. My switch to coding is thanks to the <a className={css.link} href="https://codefirstgirls.org.uk/">Code First: Girls</a>  Intro to Web Development course. Having previously managed student experience projects and initiatives at the University of Birmingham, I am interested in using code to creatively solve problems and build positive user and developer experiences. </p>
     </div>
